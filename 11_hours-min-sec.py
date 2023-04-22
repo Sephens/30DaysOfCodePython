0c8e26ebfd51eb3297064d70369698f7237493c5
Sephens <adesephens@gmail.com>
2023-04-22 12:19:25 +0300

    streak 11 to 20

---
+++ 11_hours-min-sec.py
@@ -0,0 +1,6 @@
+"""Write a getHoursMinutesSeconds() function that has a totalSeconds parameter. The
+argument for this parameter will be the number of seconds to be translated into the number of hours,
+minutes, and seconds. If the amount for the hours, minutes, or seconds is zero, don’t show it: the
+function should return '10m' rather than '0h 10m 0s'. The only exception is that
+getHoursMinutesSeconds(0) should return '0s'."""
+
